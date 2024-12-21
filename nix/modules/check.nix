fa68fe2b178cff05781c1124ef68be9cd13f2394
anntnzrb <anntnzrb@proton.me>
2024-12-21 02:37:55 -0500

    feat(pre-commit): do not manually write the check

--- nix/modules/check.nix
+++ nix/modules/check.nix
@@ -7,46 +7,41 @@
 
   perSystem =
     {
-      self',
       pkgs,
-      system,
+      config,
       ...
     }:
     {
-      checks.pre-commit = inputs.pre-commit-hooks.lib.${system}.run {
-        src = inputs.self;
+      pre-commit.settings.hooks = {
+        flake-checker.enable = true;
+        actionlint.enable = true;
 
-        hooks = {
-          flake-checker.enable = true;
-          actionlint.enable = true;
+        deadnix = {
+          enable = true;
+          settings = {
+            edit = true;
+            noUnderscore = true;
+          };
+        };
 
-          deadnix = {
+        statix =
+          let
+            cfg = (pkgs.formats.toml { }).generate "statix.toml" { disabled = disabled-lints; };
+            disabled-lints = [ "repeated_keys" ];
+          in
+          {
             enable = true;
-            settings = {
-              edit = true;
-              noUnderscore = true;
+            package = pkgs.writeShellApplication {
+              name = "statix";
+              runtimeInputs = [ pkgs.statix ];
+              text = ''
+                shift
+                exec statix check --config ${cfg} "$@"
+              '';
             };
           };
-
-          statix =
-            let
-              cfg = (pkgs.formats.toml { }).generate "statix.toml" { disabled = disabled-lints; };
-              disabled-lints = [ "repeated_keys" ];
-            in
-            {
-              enable = true;
-              package = pkgs.writeShellApplication {
-                name = "statix";
-                runtimeInputs = [ pkgs.statix ];
-                text = ''
-                  shift
-                  exec statix check --config ${cfg} "$@"
-                '';
-              };
-            };
-        };
       };
 
-      checks.default = self'.checks.pre-commit;
+      checks.default = config.checks.pre-commit;
     };
 }
